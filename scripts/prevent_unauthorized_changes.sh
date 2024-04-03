2530ac9dbeeb1585c84335514b6a67d5f0b2c931
kipling <kiplingmarich@gmail.com>
2024-04-03 11:45:02 +0300

    Update workflow to avoid commiting env

--- scripts/prevent_unauthorized_changes.sh
+++ scripts/prevent_unauthorized_changes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Assuming the environment file is named .env.prod
-ENV_FILE=".env.prod"
+# Assuming your environment file is named .env
+ENV_FILE=".env"
 
 # Check if the environment file exists
-if [ ! -f "$ENV_FILE" ]; then
-  echo "Error: $ENV_FILE found. You are not allowed to commit env files"
+if [ -f "$ENV_FILE" ]; then
+  echo "Error: $ENV_FILE found. You are not allowed to modify env files"
   exit 1
 fi
