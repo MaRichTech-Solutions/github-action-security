21c81ff3087a74f07606378d826b88acc9ae8fa3
kipling <kiplingmarich@gmail.com>
2024-04-03 11:41:44 +0300

    Update workflow to avoid commiting env

--- scripts/prevent_unauthorized_changes.sh
+++ scripts/prevent_unauthorized_changes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Get the list of modified files
-MODIFIED_FILES=$(git diff --name-only HEAD^ HEAD)
+# Assuming the environment file is named .env
+ENV_FILE=".env"
 
-# Check if the environment file (.env) is modified
-if echo "$MODIFIED_FILES" | grep -q "\.env$"; then
-  echo "Error: Unauthorized changes detected in .env file"
+# Check if the environment file exists
+if [ ! -f "$ENV_FILE" ]; then
+  echo "Error: $ENV_FILE found. You are not allowed to commit env files"
   exit 1
 fi
